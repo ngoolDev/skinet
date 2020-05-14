da129f2391c63040fe9bce502b3fa7f16eed6094
Nabeel Gool <nabeelg@saratoga.co.za>
2020-05-14 23:14:00 +0200

    End of section 6

--- Infrastructure/Data/SpecificationEvaluator.cs
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -13,10 +13,25 @@ namespace Infrastructure.Data
 
             if (spec.Criteria != null)
             {
-                query = query.Where(spec.Criteria); 
+                query = query.Where(spec.Criteria);
             }
 
-            query = spec.Includes.Aggregate(query,(current,include) => current.Include(include));
+            if (spec.OrderBy != null)
+            {
+                query = query.OrderBy(spec.OrderBy);
+            }
+
+            if (spec.OrderByDescending != null)
+            {
+                query = query.OrderByDescending(spec.OrderByDescending);
+            }
+
+            if (spec.IsPagingEnabled)
+            {
+                query = query.Skip(spec.Skip).Take(spec.Take);
+            }
+
+            query = spec.Includes.Aggregate(query, (current, include) => current.Include(include));
 
             return query;
         }
